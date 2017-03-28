17cd1c0eb54f45fd0a9238b626f7f26cbb7e2ac1
Wouter van Heeswijk <wouter.vanheeswijk@tomtom.com>
2017-03-28 17:32:52 +0200

    Added stream function

--- src/strem.js
+++ src/strem.js
@@ -3,6 +3,76 @@ import {StremLexer} from '../gen/StremLexer';
 import {StremParser} from '../gen/StremParser';
 import {StremVisitor} from '../gen/StremVisitor';
 
+function stream(producer) {
+    const listeners = [];
+    let completed = false;
+    let errored = false;
+
+    const next = (value) => {
+        if (!completed && !errored) {
+            for (const listener of listeners) {
+                if (listener.next) {
+                    listener.next(value);
+                }
+            }
+        } else {
+            throw new Error('Producer cannot call next after completion or error.');
+        }
+    };
+
+    const complete = () => {
+        if (!completed && !errored) {
+            for (const listener of listeners) {
+                if (listener.complete) {
+                    listener.complete();
+                }
+            }
+        } else {
+            throw new Error('Producer cannot call complete after completion or error.');
+        }
+    };
+
+    const error = (error) => {
+        if (!completed && !errored) {
+            for (const listener of listeners) {
+                if (listener.error) {
+                    listener.error(error);
+                }
+            }
+        } else {
+            throw new Error('Producer cannot call complete after completion or error.');
+        }
+    };
+
+    const subscribe = () => {
+        producer.start({next, complete, error});
+    };
+
+    const unsubscribe = () => {
+
+    };
+
+    return {
+        subscribe: (listener) => {
+            listeners.push(listener);
+            if (listeners.length === 1) {
+                subscribe();
+            }
+            return {
+                unsubscribe: () => {
+                    const index = listeners.indexOf(listener);
+                    if (index >= 0) {
+                        listeners.splice(index, 1);
+                        if (listeners.length === 0) {
+                            unsubscribe();
+                        }
+                    }
+                }
+            };
+        }
+    };
+}
+
 class Visitor extends StremVisitor {
     visitProgram(context) {
         return this.visit(context.children[0]);
