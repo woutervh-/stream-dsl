39c5c51b3ebcddbca1388f2c5444b4d1a74df6b1
Wouter van Heeswijk <woutervh@gmail.com>
2017-04-01 10:22:33 +0200

    CodeMirror styled

--- examples/main.js
+++ examples/main.js
@@ -1,8 +1,9 @@
 import strem from '../src/strem';
 import 'codemirror/lib/codemirror.css';
+import 'codemirror/theme/abcdef.css';
 import CodeMirror from 'codemirror';
 
-const myCodeMirror = CodeMirror(document.body, {value: 'foobar'});
+const myCodeMirror = CodeMirror(document.getElementById('container'), {value: 'foobar', theme: 'abcdef', lineNumbers: true});
 
 const myStream = strem`(1.5 | 2, 2.5 -> delay 1s -> map ${x => x * 2} | 1, 2 -> delay 0.5s) -> filter ${x => x % 2 === 1} -> map ${x => x - 1}`;
 
